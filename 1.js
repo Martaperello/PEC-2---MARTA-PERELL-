de9e180727f1a13ae844555a8cb5044658949249
Marta Perelló <100278106+Martaperello@users.noreply.github.com>
2023-01-18 13:31:53 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,18 +1,4 @@
 
-          function CambiaColor() {
-          document.getElementById("cambio").style.color = "red";       
-          }
-
-            var express = require('express');
-            var app = express();
-            
-            app.get('/', function (req, res) {
-              res.send('Bienvenid@ a mi página');
-            });
-            
-            app.listen(3000, function () {
-              console.log('Tu mensaje está en el puerto 3000');
-            });
 
             
 
